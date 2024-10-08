6e6359dfa99dbfaa438074661fc78dfb0e4120f5
venuscyr <venus.amako@epitech.digital>
2024-10-08 14:32:20 +0200

    ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,13 @@
+let currentRotation = 0
+
+function rotateCircle(step) {
+   
+   const circle = document.getElementById('circle');
+    
+    currentRotation += step;
+    
+    circle.style.transform = `rotate(${currentRotation}deg)`;
+                       
+                              }
+ document.querySelectorAll(".rotate-button").forEach(button => button.addEventListener("click",()=> rotateCircle(parseInt(button.value))))
 
